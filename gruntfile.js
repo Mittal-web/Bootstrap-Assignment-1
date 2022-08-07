6b69302c36d25145f3699c8a1c40db8ec79f1835
Mittal-web <sauravmittal988@gmail.com>
2022-08-07 11:27:10 +0530

    Grunt part 1

---
+++ gruntfile.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = function (grunt) {
+    require('time-grunt')(grunt);
+    require('jit-grunt')(grunt);
+    const sass = require('node-sass');
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        sass: {
+            options: {
+                implementation: sass,
+                sourcemap: true
+            },
+            dist: {
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './'
+                    }
+                }
+            }
+        }
+    });
+    grunt.registerTask('css', ['sass'])
+    grunt.registerTask('default', ['browserSync', 'watch']);
+
+}
